a7724e537d1ed3e431349889706793b572d953e2
Ryan Rodriguez <ryan.and.rodriguez@gmail.com>
2024-11-08 21:22:21 -0800

    first

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,25 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "visual-studio-code"
+
+  # Communication Tools
+  "discord"
+  "slack"
+  "zoom"
+
+  # Utility Tools
+  "syncthing"
+
+  # Entertainment Tools
+  "vlc"
+  "steam"
+
+  # Productivity Tools
+  "raycast"
+
+  # Browsers
+  "google-chrome"
+]
---
+++ modules/shared/packages.nix
@@ -0,0 +1,61 @@
+{ pkgs }:
+
+with pkgs; [
+  # General packages for development and system management
+  aspell
+  aspellDicts.en
+  bash-completion
+  bat
+  btop
+  coreutils
+  direnv
+  killall
+  kitty
+  neofetch
+  openssh
+  sqlite
+  wget
+  zip
+
+  # Encryption and security tools
+  age
+  age-plugin-yubikey
+  gnupg
+  libfido2
+
+  # Cloud-related tools and SDKs
+  docker
+  docker-compose
+
+  # Media-related packages
+  dejavu_fonts
+  ffmpeg
+  fd
+  font-awesome
+  hack-font
+  noto-fonts
+  noto-fonts-emoji
+  meslo-lgs-nf
+
+  # Node.js development tools
+  nodePackages.npm # globally install npm
+  nodePackages.prettier
+  nodejs
+
+  # Text and terminal utilities
+  htop
+  hunspell
+  iftop
+  jetbrains-mono
+  jq
+  ripgrep
+  tree
+  tmux
+  unrar
+  unzip
+  zsh-powerlevel10k
+
+  # Python packages
+  python312
+  python312Packages.virtualenv # globally install virtualenv
+]
