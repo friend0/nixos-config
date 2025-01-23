43f290dff8573c92dba90ee8944ec1916ef0ff53
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2025-01-22 21:20:09 -0800

    duplicate option?

--- modules/nixos/vmware-guest.nix
+++ modules/nixos/vmware-guest.nix
@@ -17,15 +17,6 @@ in
     (mkRenamedOptionModule [ "services" "vmwareGuest" ] [ "virtualisation" "vmware" "guest" ])
   ];
 
-  options.virtualisation.vmware.guest = {
-    enable = mkEnableOption "VMWare Guest Support";
-    headless = mkOption {
-      type = types.bool;
-      default = false;
-      description = "Whether to disable X11-related features.";
-    };
-  };
-
   config = mkIf cfg.enable {
     assertions = [ {
       assertion = pkgs.stdenv.isi686 || pkgs.stdenv.isx86_64 || pkgs.stdenv.isAarch64;
